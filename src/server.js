395c593203fa696b3795391574c6a5af36444746
tomasgvgt <tomasgvg.t@gmail.com>
2022-12-14 13:17:20 -0500

    Socket signals for nickname, number of rounds, create new game and join room join room

--- src/server.js
+++ src/server.js
@@ -11,6 +11,22 @@ console.log(PORT);
 io.on('connection', (client)=>{
     console.log(`client ${client.id} connected`);
 
+    client.on('nickname', (nickname)=>{
+        //nickname is saved in the database.
+    });
+    client.on('number-of-rounds', (numberOfRounds)=>{
+        //number of rounds is saved in the database.
+    })
+    client.on('create-new-game', ()=>{
+        let roomId = client.id;
+        io.to(client.id).emit('room-id', client.id);
+        //room id saved to database
+        //client id saved to database
+    })
+    client.on('join-room', (roomId)=>{
+        client.join(roomId);
+        io.to(client.id).emit('you-joined', roomId);
+    })
     client.on('disconnect', ()=>{
         console.log(`client ${client.id} disconnected`);
     })
