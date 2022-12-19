6ff0b1576e2ff5f68da5533e6b983330c394fdbf
tomasgvgt <tomasgvg.t@gmail.com>
2022-12-19 18:10:23 -0500

    Create signal guess-sonata Create signal you-win-round Create signal player-won-round Create signal producer-wins-round

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ PORT = process.env.PORT;
 console.log(PORT);
 
 io.on('connection', (client)=>{
+    let roundWinner;
     console.log(`client ${client.id} connected`);
     client.on('create-new-game', (data)=>{
         let roomId = client.id;
@@ -26,14 +27,30 @@ io.on('connection', (client)=>{
         //save data.nickname to database
     })
     client.on('send-sonata', (sonata)=>{
+        roundWinner = false;
         //saves sonata to database
         console.log('The sonata is: ', sonata)
         client.broadcast.emit('receive-sonata', sonata)
+    })
+    client.on('guess-sonata', (sonataGuess)=>{
+        //compare sonata with the one in database
+        //if sonataGuess === sonata && roundWinner === false
+            roundWinner = true;
+            io.to(client.id).emit('you-win-round')
+            client.broadcast.emit('player-won-round', client.id);
+            //make changes in database
+            //send the current state of the game to all the players
+
+    })
+    client.on('producer-wins-rownd', ()=>{
+        //make changes in database
+        //send the current state of the game to all the players
     })
     client.on('disconnect', ()=>{
         console.log(`client ${client.id} disconnected`);
     })
 })
+
 
 app.get('/producer', (req, res)=>{
     res.sendFile(path.join(__dirname, 'producer.html'))
