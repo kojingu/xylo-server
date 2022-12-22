7b694866b39004eff21a82fe65eefe7275018e2d
tomasgvgt <60365542+tomasgvgt@users.noreply.github.com>
2022-12-22 12:43:40 -0500

    Server with sockets (#2)
    * create a page for producer and anotherone for player to eperiment
    organize create-new-game and join-room signal
    
    * Player roomId and nickName
    
    * Send and receive sonata sockets
    
    * Create signal guess-sonata
    Create signal you-win-round
    Create signal player-won-round
    Create signal producer-wins-round

--- src/server.js
+++ src/server.js
@@ -9,33 +9,55 @@ PORT = process.env.PORT;
 console.log(PORT);
 
 io.on('connection', (client)=>{
+    let roundWinner;
     console.log(`client ${client.id} connected`);
-
-    client.on('nickname', (nickname)=>{
-        //nickname is saved in the database.
-    });
-    client.on('number-of-rounds', (numberOfRounds)=>{
-        //number of rounds is saved in the database.
-    })
-    client.on('create-new-game', ()=>{
+    client.on('create-new-game', (data)=>{
         let roomId = client.id;
-        io.to(client.id).emit('room-id', client.id);
+        io.to(client.id).emit('room-id', roomId);
         //room id saved to database
         //client id saved to database
+        //nickname is saved to database
+        //courrent round set to 1
+        //number of players?
     })
-    client.on('join-room', (roomId)=>{
-        client.join(roomId);
-        io.to(client.id).emit('you-joined', roomId);
+    client.on('join-room', (data)=>{
+        client.join(data.roomId);
+        io.to(client.id).emit('you-joined', data.roomId);
+        //save client.id to database
+        //save data.nickname to database
+    })
+    client.on('send-sonata', (sonata)=>{
+        roundWinner = false;
+        //saves sonata to database
+        console.log('The sonata is: ', sonata)
+        client.broadcast.emit('receive-sonata', sonata)
+    })
+    client.on('guess-sonata', (sonataGuess)=>{
+        //compare sonata with the one in database
+        //if sonataGuess === sonata && roundWinner === false
+            roundWinner = true;
+            io.to(client.id).emit('you-win-round')
+            client.broadcast.emit('player-won-round', client.id);
+            //make changes in database
+            //send the current state of the game to all the players
+
+    })
+    client.on('producer-wins-rownd', ()=>{
+        //make changes in database
+        //send the current state of the game to all the players
     })
     client.on('disconnect', ()=>{
         console.log(`client ${client.id} disconnected`);
     })
 })
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'index.html'))
-});
 
+app.get('/producer', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'producer.html'))
+});
+app.get('/player', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'player.html'))
+});
 server.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`);
 });
