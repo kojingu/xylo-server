2e26e4b512fed4f79d234aea64b4144de0091cbc
tomasgvgt <60365542+tomasgvgt@users.noreply.github.com>
2023-01-24 11:17:20 -0500

    Send sonata (#8)
    * Modify game model
    
    * Move socket connection to sockets/index
    
    * Separating create_new_game feature into independant file
    
    * Build create-new-game signal
    
    * Create joinRoom function
    Create join-room signal
    Create player-joined signal
    Create you-joined signal
    
    * Create addSonata method in game.storage
    Create sendSonata function in send_sonata.socket
    Create send-sonata signal
    
    * Create signal receive-sonata

---
+++ src/sockets/send_sonata.socket.js
@@ -0,0 +1,14 @@
+const db = require('../storage');
+
+async function sendSonata(client, data){
+    data = JSON.parse(data);
+    const sonataData = {
+        current_sonata: data.sonata,
+        socket_id: client.id
+    }
+    await db.gameStorage.addSonata(sonataData);
+    return data.sonata;
+}
+
+
+module.exports = sendSonata;
--- src/storage/game.storage.js
+++ src/storage/game.storage.js
@@ -1,10 +1,23 @@
 const Game = require('../db/models/game.model');
+const Player = require('../db/models/player.model')
 
 class GameStorage{
     async createNewGame(newGameData){
         const newGame = new Game(newGameData);
         await newGame.save();
         return newGame;
+    }
+    async addSonata(data){
+        const current_sonata = data.current_sonata;
+        const socket_id = data.socket_id;
+        const player = await Player.findOne({socket_id})
+        const io_room_id = player.io_room_id;
+        await Game.updateOne({io_room_id},
+        {
+            current_sonata,
+            current_producer_id: socket_id
+        }
+        )
     }
 }
 
