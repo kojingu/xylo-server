60fc9fbe25081d7c98d732068e820edeb021df7b
tomasgvgt <tomasgvg.t@gmail.com>
2023-02-09 11:38:19 -0500

    Set env variables=

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,16 @@
-//Connect to mongo database using mongoose
 const mongoose = require('mongoose');
-//const {DB_HOST, DB_USER, DB_PASS} = require('./config')
-const uri = `mongodb://root:password@localhost:27017/`;
-//mongodb://root:password@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false
+require('dotenv').config();
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/`;
 
 function connectToDatabase(){
+    console.log(uri);
     mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> console.log('[db] Succesfully connected'))
     .catch((err)=>console.error(`[db] ${err}`));
 }
 
 module.exports = connectToDatabase;
-
-
-
